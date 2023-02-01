0ec4190066dfa1d1c9754fbe1d2185ce54e40367
Yaya Zhang <93340455+duckduckyaya@users.noreply.github.com>
2023-02-01 22:38:18 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker images
+docker build -f dags/immo_eliza_pipeline/scraper/Dockerfile -t airflow_scraper:latest ./dags/immo_eliza_pipeline;
+
+# Init Airflow (Only first time to install everything)
+docker compose up airflow-init;
+
+# Run Airflow
+docker-compose --env-file .env up -d;
